35ce3861a0484b22ff68456fbc190ffea78701cc
Dipak giri <dipakgiri.dev@gmail.com>
2026-07-23 17:57:27 +0545

    refactor StudentInfo component for improved styling and layout consistency

--- src/components/grade-sheet/components/StudentInfo.tsx
+++ src/components/grade-sheet/components/StudentInfo.tsx
@@ -24,43 +24,49 @@ export default function StudentInfo({
   examName,
 }: StudentInfoProps) {
   const labelStyle: React.CSSProperties = {
-    whiteSpace: "nowrap",
-    fontWeight: 600,
     fontSize: "13px",
+    fontWeight: 700,
+    color: "#1f5e9d",
+    fontFamily: "Arial, sans-serif",
+    whiteSpace: "nowrap",
     marginRight: "4px",
   };
 
   const underlineBase: React.CSSProperties = {
-    borderBottom: "1px solid #1f5e9d",
-    padding: "0px 4px 10px 4px",
-    fontWeight: 700,
-    whiteSpace: "nowrap",
-    fontSize: "13px",
-    textAlign: "center",
     display: "inline-block",
+    borderBottom: "1px solid #1f5e9d",
+    padding: "0px 2px 2px 2px",
+    fontSize: "13px",
+    fontFamily: "Arial, sans-serif",
+    color: "#1f5e9d",
+    fontWeight: 600,
+    flex: 1,
+    minWidth: "30px",
     lineHeight: 1.5,
+    textAlign: "center",
   };
 
   return (
     <div
       style={{
         width: "100%",
-        fontSize: "14px",
+        fontSize: "13px",
         fontWeight: 700,
         fontFamily: '"Arial", sans-serif',
         color: "#1f5e9d",
-        padding: "8px 0",
+        padding: "4px 0",
         lineHeight: "1.6",
         display: "flex",
         flexDirection: "column",
-        gap: "4px",
+        marginBottom: "8px",
+        gap: "2px",
       }}
     >
       {/* Line 1: Name + DOB BS */}
       <div
         style={{
           display: "flex",
-          alignItems: "flex-end",
+          alignItems: "center",
           gap: "8px",
           width: "100%",
         }}
@@ -69,22 +75,14 @@ export default function StudentInfo({
         <span
           style={{
             ...underlineBase,
-            flex: 2,
             minWidth: "100px",
-            textAlign: "center",
+            maxWidth: "150px",
           }}
         >
           {studentName}
         </span>
         <span style={labelStyle}>DATE OF BIRTH:</span>
-        <span
-          style={{
-            ...underlineBase,
-            flex: 1,
-            minWidth: "60px",
-            textAlign: "center",
-          }}
-        >
+        <span style={{ ...underlineBase, minWidth: "60px" }}>
           {dateOfBirth || "\u00A0"}
         </span>
         <span style={labelStyle}>B. S.</span>
@@ -94,40 +92,20 @@ export default function StudentInfo({
       <div
         style={{
           display: "flex",
-          alignItems: "flex-end",
-          gap: "8px",
+          alignItems: "center",
+          gap: "4px",
           width: "100%",
         }}
       >
         <span style={labelStyle}>(</span>
-        <span
-          style={{ ...underlineBase, minWidth: "40px", textAlign: "center" }}
-        >
+        <span style={{ ...underlineBase, minWidth: "40px" }}>
           {dateOfBirthAD || "\u00A0"}
         </span>
         <span style={labelStyle}>&nbsp;A.D.)&nbsp;&nbsp;</span>
         <span style={labelStyle}>ROLL NO:</span>
-        <span
-          style={{
-            ...underlineBase,
-            flex: 0.5,
-            minWidth: "40px",
-            textAlign: "center",
-          }}
-        >
-          {rollNo}
-        </span>
+        <span style={{ ...underlineBase, minWidth: "40px" }}>{rollNo}</span>
         <span style={labelStyle}>GRADE:</span>
-        <span
-          style={{
-            ...underlineBase,
-            flex: 0.5,
-            minWidth: "40px",
-            textAlign: "center",
-          }}
-        >
-          {grade}
-        </span>
+        <span style={{ ...underlineBase, minWidth: "40px" }}>{grade}</span>
         <span style={labelStyle}>IN THE</span>
       </div>
 
@@ -135,21 +113,17 @@ export default function StudentInfo({
       <div
         style={{
           display: "flex",
-          alignItems: "flex-end",
-          gap: "8px",
+          alignItems: "center",
+          gap: "4px",
           width: "100%",
-          marginTop: "6px",
+          marginTop: "4px",
         }}
       >
         <span style={{ ...labelStyle, fontWeight: 900, marginRight: "4px" }}>
           {examName ? examName.toUpperCase() : "EXAMINATION"}
         </span>
         <span style={labelStyle}>CONDUCTED IN</span>
-        <span
-          style={{ ...underlineBase, minWidth: "45px", textAlign: "center" }}
-        >
-          {nepaliYear}
-        </span>
+        <span style={{ ...underlineBase, minWidth: "45px" }}>{nepaliYear}</span>
         <span style={labelStyle}>B.S. ARE GIVEN BELOW.</span>
       </div>
     </div>
